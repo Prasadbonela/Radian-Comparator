6735fe11f497c3f331ebc2a2a02d4e3111f9a47c
Prasad52892 <prasadbonela52892@gmail.com>
2024-07-28 14:40:04 +0530

    Solved

--- src/main/java/edu/epam/fop/lambdas/RadianComparator.java
+++ src/main/java/edu/epam/fop/lambdas/RadianComparator.java
@@ -3,6 +3,8 @@ package edu.epam.fop.lambdas;
 import java.util.Comparator;
 
 public final class RadianComparator {
+  private static final double EPSILON = 0.001;
+
 
   /*
    * Compares 2 angles (passed in radians). The angle counts from 0 up to 2π, the period must
@@ -11,7 +13,38 @@ public final class RadianComparator {
    * 0 == 2π
    */
   public static Comparator<Double> get() {
-    // TODO write your code here
-    return null;
+
+    return (a, b) -> {
+
+      if (a == null && b == null) {
+        return 0;
+      } else if (a == null) {
+        return - 1;
+      } else if (b == null) {
+        return 1;
+      }
+
+      double normalizedA = normalize(a);
+      double normalizedB = normalize(b);
+
+
+      if (Math.abs(normalizedA - normalizedB) < EPSILON) {
+        return 0;
+      } else if (normalizedA < normalizedB) {
+        return - 1;
+      } else {
+        return 1;
+      }
+    };
+  }
+
+  private static double normalize(double radian) {
+    while (radian < 0) {
+      radian += 2 * Math.PI;
+    }
+    while (radian >= 2 * Math.PI) {
+      radian -= 2 * Math.PI;
+    }
+    return radian;
   }
 }
